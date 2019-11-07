6fbd0a363a48cb6acee7b9ba765514897d3bd37a
Harmanpreet <kaurharmanpreet502@gmail.com>
2019-11-07 15:11:23 -0500

    Adding and deleting folders

--- Harmanpreet_C0765590_Note_p1/ViewController.swift
+++ Harmanpreet_C0765590_Note_p1/ViewController.swift
@@ -8,19 +8,72 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var tableFolders: UITableView!
+    
+    var folders: [String]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        
+        folders = []
         self.navigationItem.rightBarButtonItem = self.editButtonItem
         self.navigationItem.rightBarButtonItem?.tintColor = .black
         
     }
     
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return folders?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard folders != nil else {
+            return UITableViewCell()
+        }
+        
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "")
+        cell.textLabel?.text = folders![indexPath.row]
+        cell.imageView?.image = UIImage(named: "ic_folder")
+        cell.accessoryType = .disclosureIndicator
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            
+            guard folders != nil else {
+                return
+            }
+            
+            folders!.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        guard folders != nil else {
+            return
+        }
+        
+        let movedItem = folders![sourceIndexPath.row]
+        folders!.remove(at: sourceIndexPath.row)
+        folders!.insert(movedItem, at: destinationIndexPath.row)
+    }
     
 
     /*
@@ -37,6 +90,25 @@ class ViewController: UIViewController {
         
         let alertController = UIAlertController(title: "New Folder", message: "Enter a name for this folder", preferredStyle: .alert)
         
+        alertController.addTextField { (txtNewFolder) in
+            txtNewFolder.placeholder = "Name"
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        let addItemAction = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            
+            
+            let textField = alertController.textFields![0]
+            self.folders!.append(textField.text!)
+            self.tableFolders.reloadData()
+            
+        }
+        
+        alertController.addAction(addItemAction)
+        alertController.addAction(cancelAction)
+        
+        self.present(alertController, animated: false, completion: nil)
     }
     
     
